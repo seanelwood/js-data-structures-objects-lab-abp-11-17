787576ee8128323e1d444eee9365074b6f8ab725
Sean Elwood <seanelwood@gmail.com>
2017-11-10 18:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deleteFromDriverByKey(driver, key) {
   return newObj;
 }
 
-function destructivelyDeleteFromDriverByKey(driver, key) {
+function destructivelyDeleteFromdriverByKey(driver, key) {
   delete driver.key;
   return driver;
 }
