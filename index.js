13ee09ba6f7bf35bfa8082d3b48a4bb2560710db
Sean Elwood <seanelwood@gmail.com>
2017-11-10 17:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Write your solution in this file!
 const driver = {};
 
-
 // updateDriverWithKeyAndValue()- this function should take in a driver object, a key and a value. The function should not mutate the driver parameter and return a new driver that has an updated value for the key passed in.
 function updateDriverWithKeyAndValue(driver, key, value) {
   const newObj = { ...driver };
   newObj[key] = value;
-
   return newObj;
 }
+
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   
 }
